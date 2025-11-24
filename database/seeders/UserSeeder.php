e9f78462c99091ce3733ed377c950b9db05366a3
sBugraTiryaki <sbugratiryaki@gmail.com>
2025-11-24 07:08:14 +0300

    Add role and permission management with Spatie package

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::firstOrCreate(
+            ['email' => 'test@example.com'],
+            [
+                'name' => 'Test User',
+                'password' => 'password',
+                'email_verified_at' => now(),
+            ]
+        );
+
+        $user->assignRole('admin');
+    }
+}
